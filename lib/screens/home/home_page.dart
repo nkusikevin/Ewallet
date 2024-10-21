62b00646642a536d54f005bc508193f01ffa1898
nkusikevin <nkusikvnhart@gmail.com>
2024-10-21 14:15:35 +0200

    init app

---
+++ lib/screens/home/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Demo Home Page'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/second');
+          },
+          child: const Text('Go to Second Page'),
+        ),
+      ),
+    );
+  }
+}
